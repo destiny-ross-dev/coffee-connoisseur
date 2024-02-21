da0d30b3da7dfe86cd37ebe98534aaba8e9d9e81
Destiny Ross <destinylaross@gmail.com>
2024-02-21 09:58:09 -0500

    update domain to match vercel deployment

--- utils/getDomain.ts
+++ utils/getDomain.ts
@@ -1,7 +1,7 @@
 export const getDomain = () => {
   return new URL(
     process.env.NODE_ENV === "production"
-      ? "https://coffee-connoisseur.vercel.app"
+      ? "https://coffee-connoisseur-orcin.vercel.app"
       : "http://localhost:3000"
   );
 };
